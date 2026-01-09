510604bbb620a13ba0ae95008127bde892160040
Sebastian <sebastianwoj1010@gmail.com>
2026-01-09 20:27:16 +0100

    Seventh commit and push

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,7 +1,14 @@
 package h2;
 
 public class H2_main {
-    public void main(String [] args){
+    public static void main(String [] args){
 
+
+          /*  Feld [][] feld = new Feld [][]{{},
+                                            {},
+                                             {}};
+        Spielbrett brett = new Spielbrett(feld, 3);
+
+           */
     }
 }
